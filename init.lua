fa36abc4ca254faea45ab649c15850b8b0344199
Gaz <dianghazy@protonmail.com>
2022-06-10 00:32:03 +0700

    add .vscode

--- init.lua
+++ init.lua
@@ -1,150 +1,149 @@
 -- init.lua
-  require('plugins')
-  require('keybinds')
-  require('coc-config')
-  require('autotag')
-  require('term')
-  require('vscode')
-  require('nvim-autopairs').setup {}
-  require('nvim_comment').setup()
+require('plugins')
+require('keybinds')
+require('coc-config')
+require('autotag')
+require('term')
+require('vscode')
+require('nvim-autopairs').setup {}
+require('nvim_comment').setup()
 
-  vim.cmd [[
+vim.cmd [[
   augroup init_user_config
     autocmd!
     autocmd BufWritePost init.lua source <afile> | so ~/.config/nvim/init.lua
   augroup end
 ]]
 
-  -- treesitter
-  require 'nvim-treesitter.configs'.setup {
-    ensure_installed = "all",
-    context_commentstring = {
-      enable = true
-    },
-    highlight = {
-      enable = true,
-      disable = { "lua" }
-    },
-    indent = {
-      enable = true
-    },
-    rainbow = {
-      enable = true,
-      extended_mode = true,
-      max_file_lines = nil,
+-- treesitter
+require 'nvim-treesitter.configs'.setup {
+  ensure_installed = "all",
+  context_commentstring = {
+    enable = true
+  },
+  highlight = {
+    enable = true,
+    disable = { "lua" }
+  },
+  indent = {
+    enable = true
+  },
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = nil,
+  }
+}
+
+-- telescope
+require('telescope').setup({
+  pickers = {
+    find_filse = {
+      theme = "vscode"
     }
   }
+})
 
-  -- telescope
-  require('telescope').setup({
-    pickers = {
-      find_filse = {
-        theme = "vscode"
-      }
-    }
-  })
+-- bufferline
+require("bufferline").setup({
+  options = {
+    buffer_close_icon = "",
+    close_command = "bdelete! %d",
+    close_icon = "",
+    indicator_icon = " ",
+    left_trunc_marker = "",
+    modified_icon = "●",
+    offsets = { { filetype = "NvimTree", text = "EXPLORER", text_align = "center" } },
+    right_mouse_command = "bdelete! %d",
+    right_trunc_marker = "",
+    show_close_icon = false,
+    show_tab_indicators = true,
+  },
+  highlight = {
+    fill = {
+      guifg = { attribute = "fg", highlight = "Normal" },
+      guibg = { attribute = "bg", highlight = "StatusLineNC" },
+    },
+    background = {
+      guifg = { attribute = "fg", highlight = "Normal" },
+      guibg = { attribute = "bg", highlight = "StatusLine" },
+    },
+    buffer_visible = {
+      gui = "",
+      guifg = { attribute = "fg", highlight = "Normal" },
+      guibg = { attribute = "bg", highlight = "Normal" },
+    },
+    buffer_selected = {
+      gui = "",
+      guifg = { attribute = "fg", highlight = "Normal" },
+      guibg = { attribute = "bg", highlight = "Normal" },
+    },
+    separator = {
+      guifg = { attribute = "bg", highlight = "Normal" },
+      guibg = { attribute = "bg", highlight = "StatusLine" },
+    },
+    separator_selected = {
+      guifg = { attribute = "fg", highlight = "Special" },
+      guibg = { attribute = "bg", highlight = "Normal" },
+    },
+    separator_visible = {
+      guifg = { attribute = "fg", highlight = "Normal" },
+      guibg = { attribute = "bg", highlight = "StatusLineNC" },
+    },
+    close_button = {
+      guifg = { attribute = "fg", highlight = "Normal" },
+      guibg = { attribute = "bg", highlight = "StatusLine" },
+    },
+    close_button_selected = {
+      guifg = { attribute = "fg", highlight = "normal" },
+      guibg = { attribute = "bg", highlight = "normal" },
+    },
+    close_button_visible = {
+      guifg = { attribute = "fg", highlight = "normal" },
+      guibg = { attribute = "bg", highlight = "normal" },
+    },
+  },
+})
 
-  -- bufferline
-  require("bufferline").setup({
-    options = {
-      buffer_close_icon = "",
-      close_command = "bdelete! %d",
-      close_icon = "",
-      indicator_icon = " ",
-      left_trunc_marker = "",
-      modified_icon = "●",
-      offsets = { { filetype = "NvimTree", text = "EXPLORER", text_align = "center" } },
-      right_mouse_command = "bdelete! %d",
-      right_trunc_marker = "",
-      show_close_icon = false,
-      show_tab_indicators = true,
-    },
-    highlight = {
-      fill = {
-        guifg = { attribute = "fg", highlight = "Normal" },
-        guibg = { attribute = "bg", highlight = "StatusLineNC" },
-      },
-      background = {
-        guifg = { attribute = "fg", highlight = "Normal" },
-        guibg = { attribute = "bg", highlight = "StatusLine" },
-      },
-      buffer_visible = {
-        gui = "",
-        guifg = { attribute = "fg", highlight = "Normal" },
-        guibg = { attribute = "bg", highlight = "Normal" },
-      },
-      buffer_selected = {
-        gui = "",
-        guifg = { attribute = "fg", highlight = "Normal" },
-        guibg = { attribute = "bg", highlight = "Normal" },
-      },
-      separator = {
-        guifg = { attribute = "bg", highlight = "Normal" },
-        guibg = { attribute = "bg", highlight = "StatusLine" },
-      },
-      separator_selected = {
-        guifg = { attribute = "fg", highlight = "Special" },
-        guibg = { attribute = "bg", highlight = "Normal" },
-      },
-      separator_visible = {
-        guifg = { attribute = "fg", highlight = "Normal" },
-        guibg = { attribute = "bg", highlight = "StatusLineNC" },
-      },
-      close_button = {
-        guifg = { attribute = "fg", highlight = "Normal" },
-        guibg = { attribute = "bg", highlight = "StatusLine" },
-      },
-      close_button_selected = {
-        guifg = { attribute = "fg", highlight = "normal" },
-        guibg = { attribute = "bg", highlight = "normal" },
-      },
-      close_button_visible = {
-        guifg = { attribute = "fg", highlight = "normal" },
-        guibg = { attribute = "bg", highlight = "normal" },
-      },
-    },
-  })
-
-  -- status line
-  require('lualine').setup {
-    options = {
-      theme = 'vscode',
-      component_separators = { left = '', right = '' },
-      section_separators = { left = '', right = '' },
-      globalstatus = true
-    },
-    sections = {
-      lualine_x = { 'filetype' }
-    }
+-- status line
+require('lualine').setup {
+  options = {
+    theme = 'vscode',
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    globalstatus = true
+  },
+  sections = {
+    lualine_x = { 'filetype' }
   }
- 
-  -- general settings
-  vim.o.termguicolors = true
-  vim.cmd([[au ColorScheme * hi WinSeparator guibg=none]]) -- Thin separator
-  vim.cmd[[set autochdir]]
-  
-  if vim.g.goneovim == 1 then
-    vim.cmd([[au ColorScheme * hi Normal ctermbg=none guibg=#1e1e2e]])
-  else
-    vim.cmd([[au ColorScheme * hi Normal ctermbg=none guibg=none]])
-  end
+}
+
+-- general settings
+vim.o.termguicolors = true
+vim.cmd([[au ColorScheme * hi WinSeparator guibg=none]]) -- Thin separator
+vim.cmd [[set autochdir]]
+
+if vim.g.goneovim == 1 then
+  vim.cmd([[au ColorScheme * hi Normal ctermbg=none guibg=#1e1e2e]])
+else
+  vim.cmd([[au ColorScheme * hi Normal ctermbg=none guibg=none]])
+end
 
 vim.g.netrw_winsize = 20
 
-  -- colorscheme
-  vim.g.vscode_style = "dark"
-  vim.g.vscode_transparent = 1
-  vim.g.vscode_italic_comment = 1
-  vim.g.vscode_disable_nvimtree_bg = true
-  vim.cmd [[colorscheme vscode]]
-  -- mouse
-  vim.cmd [[set mouse=a]]
-  vim.opt.hidden = true
+-- colorscheme
+vim.g.vscode_style = "dark"
+vim.g.vscode_transparent = 1
+vim.g.vscode_italic_comment = 1
+vim.g.vscode_disable_nvimtree_bg = true
+vim.cmd [[colorscheme vscode]]
+-- mouse
+vim.cmd [[set mouse=a]]
+vim.opt.hidden = true
 
-  -- indentation
-  vim.bo.expandtab = true
-  vim.bo.shiftwidth = 4
-  vim.bo.softtabstop = 2
-  vim.opt.smartindent = true
-  vim.opt.number = true
-
+-- indentation
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 4
+vim.bo.softtabstop = 2
+vim.opt.smartindent = true
+vim.opt.number = true
