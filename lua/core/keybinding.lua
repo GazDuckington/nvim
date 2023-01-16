a6f8502ca9b2d0e35b2ac540f0f8f05acaef4138
Ghazy <dianghazy@protonmail.com>
2023-01-17 01:17:52 +0700

    oops

--- lua/core/keybinding.lua
+++ lua/core/keybinding.lua
@@ -116,13 +116,13 @@ map('n', ']c', function()
 	if vim.wo.diff then return ']c' end
 	vim.schedule(function() gs.next_hunk() end)
 	return '<Ignore>'
-end, { expr = true })
+end, opts)
 
 map('n', '[c', function()
 	if vim.wo.diff then return '[c' end
 	vim.schedule(function() gs.prev_hunk() end)
 	return '<Ignore>'
-end, { expr = true })
+end, opts)
 
 -- lsp stuff
 map("n", "<leader>tr", "<cmd>Telescope lsp_references<cr>", opts)
