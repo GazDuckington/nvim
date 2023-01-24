f324fd3080f3aded7aec6d237c3de0ab59f0bc10
Ghazy <dianghazy@protonmail.com>
2023-01-24 15:49:54 +0700

    up

--- lua/config/whichkey.lua
+++ lua/config/whichkey.lua
@@ -8,6 +8,9 @@ whichkey.setup({
 		border = "single"
 	},
 	layout = {
-		spacing = 5,
+		height = { min = 4, max = 25 },
+		width = { min = 20, max = 50 },
+		spacing = 3,
+		align = "left",
 	}
 })
