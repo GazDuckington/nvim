17272d5247d641a9a9f26e6927e2d2200906788b
Ghazy <dianghazy@protonmail.com>
2023-02-28 16:42:29 +0700

    Add: quarto

--- lua/config/mason.lua
+++ lua/config/mason.lua
@@ -1,22 +1,22 @@
 local mason = require("mason")
 local mason_lspconfig = require("mason-lspconfig")
 
-local servers = vim.g.must_lsp
+-- local servers = vim.g.must_lsp
 mason_lspconfig.setup({
-		ensure_installed = servers,
-		automatic_installation = true,
+	-- ensure_installed = servers,
+	automatic_installation = true,
 })
 
 local settings = {
-		ui = {
-				border = "rounded",
-				icons = {
-						package_installed = "◍",
-						package_pending = "◍",
-						package_uninstalled = "◍",
-				},
+	ui = {
+		border = "rounded",
+		icons = {
+			package_installed = "◍",
+			package_pending = "◍",
+			package_uninstalled = "◍",
 		},
-		log_level = vim.log.levels.INFO,
-		max_concurrent_installers = 4,
+	},
+	log_level = vim.log.levels.INFO,
+	max_concurrent_installers = 4,
 }
 mason.setup(settings)
