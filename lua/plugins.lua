853be0c09e5e14f83d111e721bf5fff6cb6a193b
Ghazy <dianghazy@protonmail.com>
2024-03-21 08:23:41 +0700

    modes cursorln color

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -40,6 +40,13 @@ lazy.setup(
 			end,
 		},
 		{
+			'mvllow/modes.nvim',
+			tag = 'v0.2.0',
+			config = function()
+				require('modes').setup()
+			end
+		},
+		{
 			"levouh/tint.nvim",
 		},
 		{
