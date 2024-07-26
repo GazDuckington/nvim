62eae91f9a5d87d89e8372e80ddbb31844af083e
Ghazy <dianghazy@protonmail.com>
2024-07-26 10:58:45 +0700

    add dbee

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -138,6 +138,29 @@ lazy.setup(
 				}
 			}
 		},
+		-- DB
+		{
+			"kndndrj/nvim-dbee",
+			dependencies = {
+				"MunifTanjim/nui.nvim",
+				"kndndrj/projector-dbee"
+			},
+			build = function()
+				-- Install tries to automatically detect the install method.
+				-- if it fails, try calling it with one of these parameters:
+				--    "curl", "wget", "bitsadmin", "go"
+				require("dbee").install("go")
+			end,
+			config = function()
+				require("dbee").setup(
+					{
+						outputs = {
+							require("projector_dbee").OutputBuilder:new(),
+						},
+					}
+				)
+			end,
+		},
 		-- {
 		-- 	'VonHeikemen/fine-cmdline.nvim',
 		-- 	dependencies = {
@@ -476,7 +499,6 @@ lazy.setup(
 			event = "VeryLazy",
 		},
 		-- {"averms/black-nvim", build: "UpdateRemotePlugins"},
-
 		-- markdown
 		{
 			"iamcco/markdown-preview.nvim",
